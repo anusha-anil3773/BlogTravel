0326505e6f8ff5e39739be993546b27800e0b345
anusha-anil3773 <108619912+anusha-anil3773@users.noreply.github.com>
2022-11-16 14:32:10 +0530

    commiting

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const app = new express()
 
 // require('dotenv').config()
 
-require('./middlewares/mongoDB') //to init mongoDB
-const DATA = require('../models/blog')
+// require('./middlewares/mongoDB') //to init mongoDB
+// const DATA = require('../models/blog')
 
 app.use(cors()) //to connect frontend and backend without any disturbance
 app.use(express.json()) // to recieve data from front end
@@ -20,6 +20,47 @@ app.use(logger('dev'))
 app.use(express.static('./dist/frontend'));
 
 
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const blogSchema = new Schema({
+    placename: {
+        type: String,
+        required: true
+    },
+    follow: {
+        type: Number,
+        required: true
+    },
+    articlecontent: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+
+let blogDATA = mongoose.model('article', blogSchema)
+
+
+mongoose.connect("mongodb+srv://Anusha37:anusha123@cluster0.bd302zx.mongodb.net/Article?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}). then(()=>{
+    console.log('mongoDB connected successfully!!!')
+}).catch((error)=>{
+    console.log(error.message)
+})
+
+const router = express.Router() //routing function
 
 
 // students full list read 
